bd07d8c05b16ffb2bb6d7c7b1898e5934fb37045
jenni-arias <arias.jenni@gmail.com>
2018-03-03 18:45:11 +0100

    Layout Exercise. Creación Toolbar con botón Back y Check.

--- app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
+++ app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
@@ -2,12 +2,33 @@ package arias.jenifer.juegosumas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class ExerciseActivity extends AppCompatActivity {
+
+    Toolbar toolbar_exercise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        //Configuración actionbar - toolbar
+        toolbar_exercise = (Toolbar) findViewById(R.id.toolbar_exercise);
+        setSupportActionBar(toolbar_exercise);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back);
+        getSupportActionBar().setTitle("Ejercicio");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_exercise, menu);
+        return true;
     }
 }
