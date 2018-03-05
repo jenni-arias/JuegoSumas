df93c5748269da5f571dd64dea71c584e40de6a8
jenni-arias <arias.jenni@gmail.com>
2018-03-05 14:13:10 +0100

    ExerciseActivity. Ocultar teclado y cursor en los EditTexts.

--- app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
+++ app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
@@ -4,19 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class ExerciseActivity extends AppCompatActivity {
 
     Toolbar toolbar_exercise;
+    EditText res_unid;
+    EditText res_dec;
+    EditText res_cent;
+    EditText res_mil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        res_unid = (EditText) findViewById(R.id.res_unid);
+        res_dec = (EditText) findViewById(R.id.res_dec);
+        res_cent = (EditText) findViewById(R.id.res_cent);
+        res_mil = (EditText) findViewById(R.id.res_mil);
 
         //Intent SumActivity
         Intent intent = getIntent();
@@ -30,6 +41,11 @@ public class ExerciseActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.back);
         getSupportActionBar().setTitle(title);
 
+        //Ocultar teclado
+        hideKeyboard(res_unid);
+        hideKeyboard(res_dec);
+        hideKeyboard(res_cent);
+        hideKeyboard(res_mil);
     }
 
     //Inflamos el menú en la Toolbar
@@ -50,5 +66,10 @@ public class ExerciseActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //Ocultar teclado
+    private void hideKeyboard(EditText editText) {
+        editText.setInputType(InputType.TYPE_NULL);
     }
 }
