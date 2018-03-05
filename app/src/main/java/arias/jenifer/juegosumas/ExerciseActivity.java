8c00f3ed839f68741ff8395432f84d5470215920
jenni-arias <arias.jenni@gmail.com>
2018-03-05 13:32:01 +0100

    ExerciseActivity. Método para las acciones del Toolbar, implementado botón para volver atrás.

--- app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
+++ app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 public class ExerciseActivity extends AppCompatActivity {
@@ -31,10 +32,23 @@ public class ExerciseActivity extends AppCompatActivity {
 
     }
 
+    //Inflamos el menú en la Toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_exercise, menu);
         return true;
+    }
+
+    // Acciones para el menú del Toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
