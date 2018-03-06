2bfb76f708dd6ca3b584a0dc309236a88989f373
jenni-arias <arias.jenni@gmail.com>
2018-03-06 10:20:21 +0100

    Level. Generamos las sumas con las características de cada nivel. Se elimina la carpeta "levels".

--- app/src/main/java/arias/jenifer/juegosumas/Level.java
+++ app/src/main/java/arias/jenifer/juegosumas/Level.java
@@ -30,12 +30,62 @@ class Level {
     int[] generateDown() { return generateDigits(this.numDigitsDown); } // FIXME: Esto es incorrecto ahora
 
     // TABLA CON TODOS LOS NIVELES
-    // TODO: Rellenar con los niveles según la tabla que te pasé. El acarreo
-    // TODO: es una tabla porque hay ciertas cifras que lo tienen y ciertas no.
-    static Level[] ALL_LEVELS = {
-        new Level(1, 1, new boolean[]{ false }),
-        new Level(2, 1, new boolean[]{ false, false }),
-        new Level(3, 1, new boolean[]{ false, false, false }),
-        new Level(2, 2, new boolean[]{ false, false }),
+        static Level[] ALL_LEVELS = {
+            //Bloque 1: 1 suma, sin acarreo
+            new Level(1, 1, new boolean[]{ false }),
+            new Level(1, 2, new boolean[]{ false, false }),
+            new Level(2, 1, new boolean[]{ false, false }),
+            new Level(3, 1, new boolean[]{ false, false, false }),
+            new Level(1, 3, new boolean[]{ false, false, false }),
+            //Bloque 2: 2 sumas, sin acarreo
+            new Level(2, 2, new boolean[]{ false, false }),
+            new Level(2, 3, new boolean[]{ false, false, false }),
+            new Level(3, 2, new boolean[]{ false, false, false }),
+            new Level(4, 2, new boolean[]{ false, false, false, false }),
+            new Level(2, 4, new boolean[]{ false, false, false, false }),
+            //Bloque 3: 1 suma, 1 acarreo
+            new Level(1, 1, new boolean[]{ true }),
+            new Level(2, 1, new boolean[]{ true, false }),
+            new Level(1, 2, new boolean[]{ true, false }),
+            new Level(3, 1, new boolean[]{ true, false, false }),
+            new Level(1, 3, new boolean[]{ true, false, false }),
+            //Bloque 4: 2 sumas, 1 acarreo
+            new Level(2, 2, new boolean[]{ true, false }),
+            new Level(2, 3, new boolean[]{ true, false, false }),
+            new Level(3, 2, new boolean[]{ true, false, false }),
+            new Level(4, 2, new boolean[]{ true, false, false, false }),
+            new Level(2, 4, new boolean[]{ true, false, false, false }),
+            new Level(2, 2, new boolean[]{ false, true }),
+            new Level(2, 3, new boolean[]{ false, true, false }),
+            new Level(3, 2, new boolean[]{ false, true, false }),
+            new Level(4, 2, new boolean[]{ false, true, false, false }),
+            new Level(2, 4, new boolean[]{ false, true, false, false }),
+            //Bloque 5: 3 sumas, sin acarreo
+            new Level(3, 3, new boolean[]{ false, false, false }),
+            new Level(4, 3, new boolean[]{ false, false, false, false }),
+            new Level(3, 4, new boolean[]{ false, false, false, false }),
+            //Bloque 6: 3 sumas, 1 acarreo
+            new Level(3, 3, new boolean[]{ true, false, false }),
+            new Level(4, 3, new boolean[]{ true, false, false, false }),
+            new Level(3, 4, new boolean[]{ true, false, false, false }),
+            new Level(3, 3, new boolean[]{ false, true, false }),
+            new Level(4, 3, new boolean[]{ false, true, false, false }),
+            new Level(3, 4, new boolean[]{ false, true, false, false }),
+            new Level(3, 3, new boolean[]{ false, false, true }),
+            new Level(4, 3, new boolean[]{ false, true, false, false }),
+            new Level(3, 4, new boolean[]{ false, false, true, false }),
+            //Bloque 7: 3 sumas, 2 acarreos
+            new Level(3, 3, new boolean[]{ true, false, true }),
+            new Level(4, 3, new boolean[]{ true, false, true, false }),
+            new Level(3, 4, new boolean[]{ true, false, true, false }),
+            //Bloque 8: 3 sumas, 2 acarreos
+            new Level(2, 2, new boolean[]{ true, true }),
+            new Level(2, 3, new boolean[]{ true, true, false }),
+            new Level(3, 2, new boolean[]{ true, true, false }),
+            //Bloque 9: 3 sumas, 3 acarreo
+            new Level(3, 3, new boolean[]{ true, true, true }),
+            new Level(4, 3, new boolean[]{ true, true, true }),
+            new Level(3, 4, new boolean[]{ true, true, true }),
+
     };
 }
