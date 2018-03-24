2f23af9ad6fd3d848c1efd8041430ae0ceb900ee
jenni-arias <arias.jenni@gmail.com>
2018-03-24 16:26:31 +0100

    Level y ExerciseActivity. Limpieza de código.

--- app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
+++ app/src/main/java/arias/jenifer/juegosumas/ExerciseActivity.java
@@ -1,5 +1,6 @@
 package arias.jenifer.juegosumas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,273 +16,9 @@ import android.widget.Toast;
 
 import java.util.Locale;
 
+
 /******************************
  *          FORMA 1
- *
- *****************************/
-/*
-public class ExerciseActivity extends AppCompatActivity
-{
-    private String prefixes[] = { "unid", "dec", "cent", "mil" };
-    private Level level;
-    private int levelIndex;
-
-    private Toolbar toolbar_exercise;
-    private TextView digitsUp[], digitsDown[];
-    private EditText results[], carry[];
-    private int[] numbersUp;
-    private int[] numbersDown;
-    private boolean[] posCarry;
-    private boolean acceptCarry;
-
-    private int getId(String digit, String position) {
-        String name = String.format("%s_%s", digit, position);
-        return getResources().getIdentifier(name, "id", getPackageName());
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercise);
-
-        digitsUp = new TextView[4];
-        digitsDown = new TextView[4];
-        results = new EditText[4];
-        carry = new EditText[4];
-        for (int i = 0; i < prefixes.length; i++) {
-            digitsUp[i] = (TextView) findViewById(getId(prefixes[i],"up"));
-            digitsDown[i] = (TextView) findViewById(getId(prefixes[i], "down"));
-            results[i] = (EditText) findViewById(getId(prefixes[i], "res"));
-            carry[i] = (EditText) findViewById(getId(prefixes[i], "carry"));
-        }
-
-        //Intent SumActivity
-        levelIndex = getIntent().getIntExtra("Nivel", -1);
-        int showNum = levelIndex + 1;
-        String title = String.format(Locale.getDefault(), "Ejercicio Nivel %d", showNum);
-
-        // Obtenemos el nivel y le pedimos que nos genere unos números concretos
-        // a partir de la plantilla
-        level = Level.ALL_LEVELS[levelIndex];
-        numbersUp = level.generateUp();
-        numbersDown = level.generateDown();
-
-        //Comprobar si el nivel acepta acarreo o no
-        acceptCarry = level.acceptCarry();
-
-        //Obtener las posiciones de los acarreos
-        posCarry = level.posCarry();
-
-        //Comprobar si la suma tiene los acarreos en las posiciones correctas
-        boolean correctCarry = checkCarry(numbersUp, numbersDown, acceptCarry, posCarry);
-
-        if (correctCarry) {
-            //Mostrar ejercicio dependiendo de los datos que se reciben
-            //TODO: Comprobar que los números no empiezan por 0
-            setDigits(digitsUp, numbersUp);
-            setDigits(digitsDown, numbersDown);
-
-            //Mostrar / Ocultar los EditText del resultado según acarreo del nivel.
-            hideEditResult(numbersUp, numbersDown, posCarry, results);
-            hideEditCarry(posCarry, carry);
-        } else {
-            recreate();
-        }
-
-        //Configuración actionbar - toolbar
-        toolbar_exercise = (Toolbar) findViewById(R.id.toolbar_exercise);
-        setSupportActionBar(toolbar_exercise);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back);
-        getSupportActionBar().setTitle(title);
-
-        //Ocultar teclado del móvil
-        for (EditText e : results) {
-            hideKeyboard(e);
-        }
-        for (EditText e : carry) {
-            hideKeyboard(e);
-        }
-
-        //Indicar primera posición del número a introducir
-        results[0].requestFocus();
-
-    }
-
-    //Inflamos el menú en la Toolbar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_exercise, menu);
-        return true;
-    }
-
-    // Acciones para el menú del Toolbar
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            case R.id.action_check:
-               // checkResult();
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    //Comprobar si la suma tiene los acarreos en las posiciones correctas
-    private boolean checkCarry(int[] numbersUp, int[] numbersDown, boolean acceptCarry, boolean[] posCarry) {
-        boolean correctCarry = true;
-        int max;
-
-        if (numbersUp.length <= numbersDown.length) {
-            max = numbersUp.length;
-        } else {max = numbersDown.length; }
-
-        boolean [] acarreo = new boolean[max];
-
-        if (!acceptCarry) {     //No acepta acarreo
-            for (int i = 0; i < max; i++) {
-                if (numbersUp[i] + numbersDown[i] > 9) {
-                    acarreo[i] = false;         //Incorrecto
-                }
-                else if (numbersUp[i] + numbersDown[i] < 9){
-                    acarreo[i] = true;          //Correcto
-                }
-            }
-        } else {                //Acepta acarreo
-            for (int i = 0; i < max; i++) {
-                if (numbersUp[i] + numbersDown[i] > 9 && posCarry[i]) {
-                    acarreo[i] = true;          //Correcto
-                }
-                else if (numbersUp[i] + numbersDown[i] < 9 && !posCarry[i]){
-                    acarreo[i] = true;          //Correcto
-                }
-                else {acarreo[i] = false; }     //Incorrecto
-            }
-        }
-
-        for (int i = 0; i< acarreo.length; i++) {
-            if (acarreo[i] && correctCarry) {
-                correctCarry = true;
-            } else {correctCarry = false; }
-        }
-        return correctCarry;
-    }
-
-    //Insertar los números en los TextView.
-    private static void setDigits(TextView[] digits, int[] numbers) {
-        for (int i = 0; i < digits.length; i++) {
-            if (i < numbers.length) {
-                digits[i].setText(String.valueOf(numbers[i]));
-            } else {
-                digits[i].setVisibility(View.INVISIBLE);
-            }
-        }
-    }
-
-    //Mostrar / Ocultar los EditText del resultado según acarreo del nivel.
-    private void hideEditResult(int[] numbersUp, int[] numbersDown, boolean[] posCarry, EditText results[]) {
-        int max;
-        if (numbersUp.length >= numbersDown.length) {
-            max = numbersUp.length;
-        } else {max = numbersDown.length; }
-
-        for (int i = max; i < results.length; i++) {
-            results[i].setVisibility(View.INVISIBLE);
-        }
-
-        if (posCarry[posCarry.length-1]) {
-            results[posCarry.length].setVisibility(View.VISIBLE);
-        }
-    }
-
-    //Mostrar / Ocultar los EditText del acarreo según el nivel.
-    private void hideEditCarry(boolean[] posCarry, EditText[] carry) {
-
-        //TODO: se repite, crear método
-        boolean[] pos = new boolean[4];
-        for (int i = 0; i < posCarry.length; i++) {
-            pos[i] = posCarry[i];
-        }
-
-        carry[0].setVisibility(View.INVISIBLE);
-        for (int i = 1; i < pos.length; i++) {
-            if(pos[i-1]) {
-                carry[i].setVisibility(View.VISIBLE);
-            } else {
-                carry[i].setVisibility(View.INVISIBLE);
-            }
-        }
-    }
-
-    //Ocultar teclado del móvil
-    private void hideKeyboard(EditText editText) {
-        editText.setInputType(InputType.TYPE_NULL);
-    }
-
-    //Captura click del número seleccionado
-    public void clicat(View view) {
-        Button boton = (Button) view;
-        int num = Integer.parseInt(boton.getText().toString());
-        setResults(results, carry, posCarry, num);
-    }
-
-    //Insertar número seleccionado en el EditText y organización del Focus.
-    //TODO: Falta deshabilitar el último EditText una vez introducido todos los números
-    public void setResults (EditText results[], EditText carry[], boolean[] posCarry, int num) {
-        String number = String.valueOf(num);
-
-        //TODO: se repite, crear método
-        boolean[] pos = new boolean[4];
-        for (int i = 0; i < posCarry.length; i++) {
-            pos[i] = posCarry[i];
-        }
-
-        if (results[0].hasFocus()) {
-           results[0].setText(number);
-            if(pos[0]) {
-                carry[1].requestFocus();
-            } else { results[1].requestFocus(); }
-        }
-        else if (carry[1].hasFocus()) {
-            carry[1].setText(number);
-            results[1].requestFocus();
-        }
-        else if (results[1].hasFocus()) {
-           results[1].setText(number);
-           if(pos[1]) {
-               carry[2].requestFocus();
-           } else { results[2].requestFocus(); }
-        }
-        else if (carry[2].hasFocus()) {
-            carry[2].setText(number);
-            results[2].requestFocus();
-        }
-        else if (results[2].hasFocus()) {
-           results[2].setText(number);
-            if(pos[2]) {
-                carry[3].requestFocus();
-            } else { results[3].requestFocus(); }
-        }
-        else if (carry[3].hasFocus()) {
-            carry[3].setText(number);
-            results[3].requestFocus();
-        }
-        else if (results[3].hasFocus()) {
-            results[3].setText(number);
-        }
-    }
-
-   /* private void checkResult(int[] numbersUp, int[] numbersDown, EditText results[]) {
-
-    }
-} */
-
-/******************************
- *          FORMA 2
  *
  *****************************/
 
@@ -406,10 +143,17 @@ public class ExerciseActivity extends AppCompatActivity
         switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
+              /*  Intent intent2 = new Intent(this, SumActivity.class);
+                intent2.putExtra("CurrentExercise", mCurrentExercise);
+                startActivity(intent2); */
                 return true;
             case R.id.action_check:
                 if (mCurrentExercise == 5) {
                     finish();
+                  /*  Intent intent = new Intent(this, SumActivity.class);
+                    intent.putExtra("CurrentExercise", mCurrentExercise);
+                    startActivity(intent); */
+
                 } else { checkResult(numbersUp, numbersDown, results); }
             default:
                 return super.onOptionsItemSelected(item);
@@ -694,7 +438,7 @@ public class ExerciseActivity extends AppCompatActivity
     //Establece los colores en la progressBar del nivel según si el resultado es correcto o no.
     private void setColors(int mCurrentExercise, boolean correct) {
         if (correct) {
-            for (int i = 0; i < mCurrentExercise+1; i ++) {
+            for (int i = 0; i < mCurrentExercise+1; i++) {
                 correct_result[mCurrentExercise].setBackground(getResources()
                         .getDrawable(R.drawable.progressbar_green));
             }
@@ -703,7 +447,7 @@ public class ExerciseActivity extends AppCompatActivity
                         .getDrawable(R.drawable.progressbar_yellow));
             }
         } else {
-            for (int i = 1; i < 5; i ++) {
+            for (int i = 1; i < 5; i++) {
                 correct_result[i].setBackground(getResources()
                         .getDrawable(R.drawable.progressbar_gray));
             } correct_result[mCurrentExercise].setBackground(getResources()
@@ -715,7 +459,7 @@ public class ExerciseActivity extends AppCompatActivity
 
 
 /******************************
- *          FORMA 3
+ *          FORMA 2
  *
  *****************************/
 /*
--- app/src/main/java/arias/jenifer/juegosumas/Level.java
+++ app/src/main/java/arias/jenifer/juegosumas/Level.java
@@ -1,8 +1,6 @@
 package arias.jenifer.juegosumas;
 
 
-import android.text.style.UpdateAppearance;
-
 
 /**
  * Created by j.arias.gallego on 26/02/2018.
@@ -11,113 +9,6 @@ import android.text.style.UpdateAppearance;
 
 /******************************
  *          FORMA 1
- *
- *****************************/
-
-/*
-class Level {
-    private int numDigitsUp, numDigitsDown;
-    private boolean[] carry;
-
-    private Level(int digitsUp, int digitsDown, boolean[] carry) {
-        this.numDigitsDown = digitsDown;
-        this.numDigitsUp = digitsUp;
-        this.carry = carry;
-    }
-
-    private static int[] generateDigits(int n) {
-        int[] digits = new int[n];
-        for (int i = 0; i < digits.length; i++) {
-            digits[i] = (int) (Math.random() * 9);
-        }
-        return digits;
-    }
-
-    int[] generateUp() { return generateDigits(this.numDigitsUp); }
-    int[] generateDown() { return generateDigits(this.numDigitsDown); }
-
-    //Comprobar si el nivel acepta acarreo o no
-    private static boolean acceptAcarreo(boolean[] carry) {
-        boolean acarreo = false;
-        for (int i = 0; i < carry.length; i++) {
-            if (carry[i] && !acarreo) {
-                acarreo = true;
-            } else if (!acarreo){
-                acarreo = false;
-            }
-        }
-        return acarreo;
-    }
-
-    boolean acceptCarry() { return acceptAcarreo(this.carry); }
-
-    //Posicion de los acarreos.
-    boolean[] posCarry() { return this.carry; }
-
-    // TABLA CON TODOS LOS NIVELES
-        static Level[] ALL_LEVELS = {
-            //Bloque 1: 1 suma, sin acarreo (Level 1 - 5)
-            new Level(1, 1, new boolean[]{ false }),
-            new Level(1, 2, new boolean[]{ false, false }),
-            new Level(2, 1, new boolean[]{ false, false }),
-            new Level(3, 1, new boolean[]{ false, false, false }),
-            new Level(1, 3, new boolean[]{ false, false, false }),
-            //Bloque 2: 2 sumas, sin acarreo (Level 6 - 10)
-            new Level(2, 2, new boolean[]{ false, false }),
-            new Level(2, 3, new boolean[]{ false, false, false }),
-            new Level(3, 2, new boolean[]{ false, false, false }),
-            new Level(4, 2, new boolean[]{ false, false, false, false }),
-            new Level(2, 4, new boolean[]{ false, false, false, false }),
-            //Bloque 3: 1 suma, 1 acarreo (Level 11 - 15)
-            new Level(1, 1, new boolean[]{ true }),
-            new Level(2, 1, new boolean[]{ true, false }),
-            new Level(1, 2, new boolean[]{ true, false }),
-            new Level(3, 1, new boolean[]{ true, false, false }),
-            new Level(1, 3, new boolean[]{ true, false, false }),
-            //Bloque 4: 2 sumas, 1 acarreo (Level 16 - 25)
-            new Level(2, 2, new boolean[]{ true, false }),
-            new Level(2, 3, new boolean[]{ true, false, false }),
-            new Level(3, 2, new boolean[]{ true, false, false }),
-            new Level(4, 2, new boolean[]{ true, false, false, false }),
-            new Level(2, 4, new boolean[]{ true, false, false, false }),
-            new Level(2, 2, new boolean[]{ false, true }),
-            new Level(2, 3, new boolean[]{ false, true, false }),
-            new Level(3, 2, new boolean[]{ false, true, false }),
-            new Level(4, 2, new boolean[]{ false, true, false, false }),
-            new Level(2, 4, new boolean[]{ false, true, false, false }),
-            //Bloque 5: 3 sumas, sin acarreo (Level 26 - 28)
-            new Level(3, 3, new boolean[]{ false, false, false }),
-            new Level(4, 3, new boolean[]{ false, false, false, false }),
-            new Level(3, 4, new boolean[]{ false, false, false, false }),
-            //Bloque 6: 3 sumas, 1 acarreo (Level 29 - 37)
-            new Level(3, 3, new boolean[]{ true, false, false }),
-            new Level(4, 3, new boolean[]{ true, false, false, false }),
-            new Level(3, 4, new boolean[]{ true, false, false, false }),
-            new Level(3, 3, new boolean[]{ false, true, false }),
-            new Level(4, 3, new boolean[]{ false, true, false, false }),
-            new Level(3, 4, new boolean[]{ false, true, false, false }),
-            new Level(3, 3, new boolean[]{ false, false, true }),
-            new Level(4, 3, new boolean[]{ false, false, true, false }),
-            new Level(3, 4, new boolean[]{ false, false, true, false }),
-            //Bloque 7: 3 sumas, 2 acarreos (Level 38 - 40)
-            new Level(3, 3, new boolean[]{ true, false, true }),
-            new Level(4, 3, new boolean[]{ true, false, true, false }),
-            new Level(3, 4, new boolean[]{ true, false, true, false }),
-            //Bloque 8: 3 sumas, 2 acarreos (Level 41 - 43)
-            new Level(2, 2, new boolean[]{ true, true }),
-            new Level(2, 3, new boolean[]{ true, true, false }),
-            new Level(3, 2, new boolean[]{ true, true, false }),
-            //Bloque 9: 3 sumas, 3 acarreo (Level 44 - 46)
-            new Level(3, 3, new boolean[]{ true, true, true }),
-            new Level(4, 3, new boolean[]{ true, true, true }),
-            new Level(3, 4, new boolean[]{ true, true, true }),
-
-    };
-}
-*/
-
-/******************************
- *          FORMA 2
  *
  *****************************/
 
@@ -332,7 +223,7 @@ class Level {
 
 
 /******************************
- *          FORMA 3
+ *          FORMA 2
  *
  *****************************/
 
